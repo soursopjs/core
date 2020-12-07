df3f5cae420a0e1c0e6945872ca049c22ac15a7e
David256 <davidborja456@gmail.com>
2020-12-07 10:37:17 -0500

    Add InputText

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import ProgressBar from './lib/progressbar';
 import Modal from './lib/modal';
 import Button from './lib/button';
 import Checkbox from './lib/checkbox';
+import InputText from './lib/inputs/input-text';
 
 export default {
     ProgressBar,
     Modal,
     Button,
     Checkbox,
+    InputText,
 };
