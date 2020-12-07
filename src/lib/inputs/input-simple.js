34bf7ec2b22d35acc7a48482814af262aee362ce
David256 <davidborja456@gmail.com>
2020-12-07 10:31:05 -0500

    New attribute to set the type of input

--- src/lib/inputs/input-simple.js
+++ src/lib/inputs/input-simple.js
@@ -11,15 +11,22 @@ export default class InputSimple extends DOMable {
      * @param {Piece | Element} parent The parent Piece
      * @param {Object} config The object of configurations
      * @param {Object} config.attrs The attributes of InputSimple
+     * @param {string} config.type The type of InputSimple
      */
     constructor(parent, config) {
         super(parent, config);
         this.dom = createTag('input');
+    }
+
+    build() {
         if (this.config.attrs) {
             Object.entries(
                 this.config.attrs,
             ).forEach(attr => this.dom.setAttribute(...attr));
         }
+        if (this.config.type) this.dom.type = this.config.type;
+        this.addToParent();
+        return this;
     }
 
     set value(value) {
