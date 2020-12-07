134c26221ba605a271ce596e5c43dc54f41dd211
David256 <davidborja456@gmail.com>
2020-12-07 18:02:25 -0500

    Set the path to do npm-dist well

--- config/rollup.config.prod.js
+++ config/rollup.config.prod.js
@@ -3,7 +3,7 @@ import { terser } from 'rollup-plugin-terser';
 export default {
     input: './src/main.js',
     output: {
-        file: './build/soursop-core.js',
+        file: './dist/soursop-core.min.js',
         format: 'iife',
         name: 'soursopCore',
         sourcemap: true,
